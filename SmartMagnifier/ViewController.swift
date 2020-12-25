6dbe01b9d887d8f6bc617d06434ba8d5f00897db
Paul Ruvolo <paul.ruvolo@olin.edu>
2020-12-24 23:43:20 -0500

    added initial commit

---
+++ SmartMagnifier/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  SmartMagnifier
+//
+//  Created by occamlab on 12/24/20.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
+    @IBOutlet var sceneView: ARSCNView!
+    
+    var configuration: ARWorldTrackingConfiguration!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        createARSessionConfiguration()
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        sceneView.session.delegate = self
+    }
+    
+    /// Create a new ARSession.
+    func createARSessionConfiguration() {
+        configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = [.horizontal, .vertical]
+        configuration.isAutoFocusEnabled = false
+        sceneView.delegate = self
+    }
+    
+    ///Called when there is a change in tracking state.  This is important for both announcing tracking errors to the user and also to triggering some app state transitions.
+    ///
+    /// - Parameters:
+    ///   - session: the AR session associated with the change in tracking state
+    ///   - camera: the AR camera associated with the change in tracking state
+    func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+        print("Session Changed State")
+    }
+    
+    func session(_: ARSession, didUpdate: ARFrame) {
+        print("Got a new frame")
+    }
+}
