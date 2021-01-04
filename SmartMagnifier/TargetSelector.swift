1c4aea02ce25bdcb8d6100ed0f958f10fb40d1e0
Paul Ruvolo <paullundyruvolo@gmail.com>
2021-01-04 15:23:28 -0500

    slowly getting the pieces together

--- SmartMagnifier/TargetSelector.swift
+++ SmartMagnifier/TargetSelector.swift
@@ -9,15 +9,43 @@ import Foundation
 import UIKit
 import ARKit
 
-class TargetSelector : UIViewController {
+class TargetSelector : UIViewController, UIScrollViewDelegate {
     var arFrame : ARFrame!
+    var coreImage: CIImage!
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // need to rotate this by 90 degrees
-        imageView.image = UIImage(pixelBuffer: arFrame.capturedImage)
+        coreImage = CIImage(cvPixelBuffer: arFrame.capturedImage).oriented(.right)
+        imageView.image = UIImage(ciImage: coreImage)
+        
+        scrollView.delegate = self
+        scrollView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height*0.8)
+        scrollView.backgroundColor = UIColor(red: 90, green: 90, blue: 90, alpha: 0.90)
+        scrollView.alwaysBounceVertical = false
+        scrollView.alwaysBounceHorizontal = false
+        scrollView.showsVerticalScrollIndicator = true
+        scrollView.flashScrollIndicators()
+
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = 10.0
+        
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(longPress))
+        scrollView.addGestureRecognizer(longPressGesture)
+    }
+    
+    @objc func longPress(recognizer:UILongPressGestureRecognizer){
+        switch recognizer.state {
+        case .ended:
+            print("ended", recognizer.location(in: imageView))
+        default: break
+        }
+    }
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
     }
 }
 
--- SmartMagnifier/ViewController.swift
+++ SmartMagnifier/ViewController.swift
@@ -27,6 +27,51 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
         configuration.planeDetection = [.horizontal, .vertical]
         configuration.isAutoFocusEnabled = false
         sceneView.delegate = self
+        sceneView.debugOptions = [.showWorldOrigin]
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        // Place content only for anchors found by plane detection.
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        
+        // Create a custom object to visualize the plane geometry and extent.
+        let plane = Plane(anchor: planeAnchor, in: sceneView)
+        
+        // Add the visualization to the ARKit-managed node so that it tracks
+        // changes in the plane anchor as plane estimation continues.
+        node.addChildNode(plane)
+    }
+    
+    /// - Tag: UpdateARContent
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        // Update only anchors and nodes set up by `renderer(_:didAdd:for:)`.
+        guard let planeAnchor = anchor as? ARPlaneAnchor,
+            let plane = node.childNodes.first as? Plane
+            else { return }
+        
+        // Update ARSCNPlaneGeometry to the anchor's new estimated shape.
+        if let planeGeometry = plane.meshNode.geometry as? ARSCNPlaneGeometry {
+            planeGeometry.update(from: planeAnchor.geometry)
+        }
+
+        // Update extent visualization to the anchor's new bounding rectangle.
+        if let extentGeometry = plane.extentNode.geometry as? SCNPlane {
+            extentGeometry.width = CGFloat(planeAnchor.extent.x)
+            extentGeometry.height = CGFloat(planeAnchor.extent.z)
+            plane.extentNode.simdPosition = planeAnchor.center
+        }
+        
+        // Update the plane's classification and the text position
+        if #available(iOS 12.0, *),
+            let classificationNode = plane.classificationNode,
+            let classificationGeometry = classificationNode.geometry as? SCNText {
+            let currentClassification = planeAnchor.classification.description
+            if let oldClassification = classificationGeometry.string as? String, oldClassification != currentClassification {
+                classificationGeometry.string = currentClassification
+                classificationNode.centerAlign()
+            }
+        }
+        
     }
     
     ///Called when there is a change in tracking state.  This is important for both announcing tracking errors to the user and also to triggering some app state transitions.
@@ -39,7 +84,6 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     }
     
     func session(_: ARSession, didUpdate: ARFrame) {
-        print("Got a new frame")
     }
     
     @IBAction func selectMagnifierTarget(_ sender: UIButton) {
@@ -48,10 +92,11 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
             return
         }
         if let vc = self.storyboard?.instantiateViewController(withIdentifier:"targetselector") as? TargetSelector {
+            let containerController = UINavigationController(rootViewController: vc)
             vc.arFrame = capturedFrame
             vc.modalTransitionStyle = .crossDissolve;
             vc.modalPresentationStyle = .overCurrentContext
-            self.present(vc, animated: true, completion: nil)
+            self.present(containerController, animated: true, completion: nil)
         }
     }
 }
