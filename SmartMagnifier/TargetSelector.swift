a59d7b0b46d99c3a93710139b5637b0f93de1fa1
Paul Ruvolo <paullundyruvolo@gmail.com>
2021-01-05 15:43:45 -0500

    Getting there, but still lots of bugs to work out.

--- SmartMagnifier/TargetSelector.swift
+++ SmartMagnifier/TargetSelector.swift
@@ -8,19 +8,31 @@
 import Foundation
 import UIKit
 import ARKit
+import ImageCoordinateSpace
+import Accelerate
+
+protocol TargetSelectorDelegate {
+    func didSelect3DPlanarTarget(anchor: ARPlaneAnchor, tapLocation: simd_float3)
+}
 
 class TargetSelector : UIViewController, UIScrollViewDelegate {
     var arFrame : ARFrame!
     var coreImage: CIImage!
+    var rawImageTransform: CGAffineTransform!
+    var sceneView: ARSCNView!
+    var delegate: TargetSelectorDelegate?
+
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // need to rotate this by 90 degrees
-        coreImage = CIImage(cvPixelBuffer: arFrame.capturedImage).oriented(.right)
+        // TODO: in landscape you wouldn't need to change the orientation.  This might have downstream issues when calculating raycasts
+        let desiredOrientation = CGImagePropertyOrientation.right
+        coreImage = CIImage(cvPixelBuffer: arFrame.capturedImage).oriented(desiredOrientation)
         imageView.image = UIImage(ciImage: coreImage)
+        rawImageTransform = coreImage.orientationTransform(for: desiredOrientation)
         
         scrollView.delegate = self
         scrollView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height*0.8)
@@ -40,7 +52,29 @@ class TargetSelector : UIViewController, UIScrollViewDelegate {
     @objc func longPress(recognizer:UILongPressGestureRecognizer){
         switch recognizer.state {
         case .ended:
-            print("ended", recognizer.location(in: imageView))
+            let imageSpace = imageView.contentSpace()
+            let imageCoordinate = imageSpace.convert(recognizer.location(in: imageView), from: imageView)
+            let arFrameCoordinate = imageCoordinate.applying(rawImageTransform)
+            // use negative to make sure the vector points in the direction in front of the camera (which is negative z)
+            let rayDirection = -arFrame.camera.intrinsics.inverse * simd_float3(Float(arFrameCoordinate.x), Float(arFrameCoordinate.y), 1.0)
+            
+            // TODO: not sure why this hack is necessary :(
+            let rayDirectionHack = simd_float3(-rayDirection.x, rayDirection.y, rayDirection.z)
+
+            let castOrigin = simd_float3(arFrame.camera.transform.columns.3.x,
+                                         arFrame.camera.transform.columns.3.y,
+                                         arFrame.camera.transform.columns.3.z)
+            let transformedDirectionWithExtraDimension = arFrame.camera.transform * simd_float4(rayDirectionHack.x, rayDirectionHack.y, rayDirectionHack.z, 0)
+            let castDirection = simd_float3(transformedDirectionWithExtraDimension.x,
+                                            transformedDirectionWithExtraDimension.y,
+                                            transformedDirectionWithExtraDimension.z)
+            let query = ARRaycastQuery(origin: castOrigin, direction: castDirection, allowing: .existingPlaneGeometry, alignment: .any)
+            guard let result = sceneView.session.raycast(query).first,
+                  let planeAnchor = result.anchor as? ARPlaneAnchor  else {
+                return
+            }
+            delegate?.didSelect3DPlanarTarget(anchor: planeAnchor, tapLocation: simd_float3(result.worldTransform.columns.3.x, result.worldTransform.columns.3.y, result.worldTransform.columns.3.z))
+            dismiss(animated: true)
         default: break
         }
     }
--- SmartMagnifier/ViewController.swift
+++ SmartMagnifier/ViewController.swift
@@ -8,10 +8,13 @@
 import UIKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate, TargetSelectorDelegate {
     @IBOutlet var sceneView: ARSCNView!
-    
+    @IBOutlet weak var magnifiedImage: UIImageView!
+    var createdCorners = false
     var configuration: ARWorldTrackingConfiguration!
+    var magnificationPlane: ARPlaneAnchor?
+    var magnificationTarget: simd_float3?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -84,8 +87,151 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     }
     
     func session(_: ARSession, didUpdate: ARFrame) {
+        guard let magnificationPlane = magnificationPlane, let magnificationTarget = magnificationTarget else {
+            return
+        }
+        var currentPlaneAnchor: ARPlaneAnchor?
+        // search for the plane in the current frame
+        // TODO: plane can merge with another plane, so might want to track that event happening
+        for anchor in didUpdate.anchors {
+            if anchor.identifier == magnificationPlane.identifier {
+                currentPlaneAnchor = anchor as! ARPlaneAnchor
+            }
+        }
+        guard let updatedMagnificationPlane = currentPlaneAnchor else {
+            return
+        }
+        let coreImage = CIImage(cvPixelBuffer: didUpdate.capturedImage)
+        let c = updatedMagnificationPlane.center
+        let e = updatedMagnificationPlane.extent
+        
+        let (adjustedXBounds, adjustedZBounds) = adjustBounds(camera: didUpdate.camera, coreImage: coreImage, planeTransform: updatedMagnificationPlane.transform, initialXBounds: [c.x - e.x/2, c.x + e.x/2], y: c.y, initialZBounds: [c.z - e.z/2, c.z + e.z/2])
+        
+        // TODO: this breaks when we use horizontal planes
+        
+        let vectors = getCornerVectors(camera: didUpdate.camera, coreImage: coreImage, planeTransform: updatedMagnificationPlane.transform, xBounds: adjustedXBounds, y: c.y, zBounds: adjustedZBounds)
+
+        print(vectors)
+        let cornerKeys = vectors.map { getPerspectiveCorrectionKey(v: $0, vs: vectors) }
+        let perspectiveCorrection = CIFilter(name: "CIPerspectiveCorrection")!
+        perspectiveCorrection.setValue(coreImage, forKey: kCIInputImageKey)
+        // TODO: need to calculate a rectangular section that encompasses the whole image plane by shrinking the extent of the plane
+        for (cornerKey, ciVector) in zip(cornerKeys, vectors) {
+            perspectiveCorrection.setValue(ciVector, forKey: cornerKey)
+        }
+        let outputImage = perspectiveCorrection.outputImage!.oriented(.right)
+        let finalImage = UIImage(ciImage: outputImage)
+        // TODO: image extent and destination extent do not intersect (might need to adjust this final UIImage)
+        magnifiedImage.image = finalImage
+    }
+
+    func adjustBounds(camera: ARCamera, coreImage: CIImage, planeTransform: simd_float4x4, initialXBounds: [Float], y: Float, initialZBounds: [Float])->([Float], [Float]) {
+        let vertices = getCornerVectors(camera: camera, coreImage: coreImage, planeTransform: planeTransform, xBounds: initialXBounds, y: y, zBounds: initialZBounds)
+        // TODO: try to shrink bounds while maintaining the area of the intersection of the image plane and the vertices (see: https://math.stackexchange.com/questions/141798/two-quadrilaterals-intersection-area-special-case)
+        
+        // try some different shrinks
+        // TODO: this should really be done with some sort of binary search
+        let shrinkProportions = [9/10.0, 8/10.0, 7/10.0, 6/10.0, 5/10.0, 2/10.0]
+        let xc = (initialXBounds[0] + initialXBounds[1])/2
+        let zc = (initialZBounds[0] + initialZBounds[1])/2
+        var bestXBounds = initialXBounds
+        var bestZBounds = initialZBounds
+        
+        // could shrink separately on each coordinate (distorts aspect ratio of plane though)
+        for s in shrinkProportions {
+            let proposedXBounds = [(initialXBounds[0] - xc)*Float(s) + xc, (initialXBounds[1] - xc)*Float(s) + xc]
+            let proposedZBounds = [(initialZBounds[0] - zc)*Float(s) + zc, (initialZBounds[1] - zc)*Float(s) + zc]
+
+            if checkBoundValidity(camera: camera, coreImage: coreImage, planeTransform: planeTransform, proposedXBounds: proposedXBounds, y: y, proposedZBounds: proposedZBounds, originalVertices: vertices) {
+                bestXBounds = proposedXBounds
+                bestZBounds = proposedZBounds
+                print("shrinking", s)
+            }
+        }
+        
+        return (bestXBounds, bestZBounds)
     }
     
+    func checkBoundValidity(camera: ARCamera, coreImage: CIImage, planeTransform: simd_float4x4, proposedXBounds: [Float], y: Float, proposedZBounds: [Float], originalVertices: [CIVector])->Bool {
+        var badBounds = false
+        let proposedVertices = getCornerVectors(camera: camera, coreImage: coreImage, planeTransform: planeTransform, xBounds: proposedXBounds, y: y, zBounds: proposedZBounds)
+        for (o, p) in zip(originalVertices, proposedVertices) {
+            let pOnScreen = p.x >= coreImage.extent.minX && p.x <= coreImage.extent.maxX && p.y >= coreImage.extent.minY && p.y <= coreImage.extent.maxY
+            let oOnScreen  = o.x >= coreImage.extent.minX && o.x <= coreImage.extent.maxX && o.y >= coreImage.extent.minY && o.y <= coreImage.extent.maxY
+            if o != p && (pOnScreen || oOnScreen) {
+                badBounds = true
+            }
+        }
+        return !badBounds
+    }
+    
+    func getCornerVectors(camera: ARCamera, coreImage: CIImage, planeTransform: simd_float4x4, xBounds: [Float], y:Float, zBounds: [Float])->[CIVector] {
+        var corners : [simd_float4] = []
+        for x in xBounds {
+            for z in zBounds {
+                corners.append(planeTransform * simd_float4(x, y, z, 1.0))
+            }
+        }
+        if !createdCorners {
+            for corner in corners {
+                makeBox(center: corner)
+            }
+            createdCorners = true
+        }
+        var vectors : [CIVector] = []
+        for corner in corners {
+            let cameraCoordinates = camera.transform.inverse * corner
+            let cameraCoordinatesCorrected = simd_float3(cameraCoordinates.x, -cameraCoordinates.y, -cameraCoordinates.z)
+            let homogeneousPixelCoordinates = camera.intrinsics * cameraCoordinatesCorrected
+            let pixelCoord = CGPoint(x: CGFloat(homogeneousPixelCoordinates.x / homogeneousPixelCoordinates.z), y: CGFloat(homogeneousPixelCoordinates.y / homogeneousPixelCoordinates.z))
+            vectors.append(pointToVector(pixelCoord, image: coreImage))
+        }
+        return vectors
+    }
+    
+    func getPerspectiveCorrectionKey(v: CIVector, vs: [CIVector])->String {
+        var returnValue = "input"
+        let ySorted = (vs.map { $0.y }).sorted()
+        let xSorted = (vs.map { $0.x }).sorted()
+        if v.y <= ySorted[1] {
+            returnValue += "Bottom"
+        } else {
+            returnValue += "Top"
+        }
+        if v.x <= xSorted[1] {
+            returnValue += "Left"
+        } else {
+            returnValue += "Right"
+        }
+        return returnValue
+    }
+    
+    func pointToVector(_ point:CGPoint, image:CIImage) -> CIVector {
+        return CIVector(x: point.x, y: image.extent.height - point.y)
+    }
+    
+    func makeBox(center: simd_float4) {
+        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        
+        let boxNode = SCNNode()
+        boxNode.geometry = box
+        boxNode.position = SCNVector3(simd_float3(center.x, center.y, center.z))
+        sceneView.scene.rootNode.addChildNode(boxNode)
+    }
+    
+    func didSelect3DPlanarTarget(anchor: ARPlaneAnchor, tapLocation: simd_float3) {
+        magnificationPlane = anchor
+        // TODO: might want to reconsider having this stored.  Perhaps this is just the initial target and not the current target.
+        magnificationTarget = tapLocation
+        createdCorners = false
+        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        
+        let boxNode = SCNNode()
+        boxNode.geometry = box
+        boxNode.transform = SCNMatrix4(anchor.transform)
+        sceneView.scene.rootNode.addChildNode(boxNode)
+    }
+
     @IBAction func selectMagnifierTarget(_ sender: UIButton) {
         guard let capturedFrame = sceneView.session.currentFrame else {
             // display some sort of popover
@@ -94,6 +240,8 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
         if let vc = self.storyboard?.instantiateViewController(withIdentifier:"targetselector") as? TargetSelector {
             let containerController = UINavigationController(rootViewController: vc)
             vc.arFrame = capturedFrame
+            vc.delegate = self
+            vc.sceneView = sceneView
             vc.modalTransitionStyle = .crossDissolve;
             vc.modalPresentationStyle = .overCurrentContext
             self.present(containerController, animated: true, completion: nil)
